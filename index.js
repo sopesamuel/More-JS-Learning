7c9eb94dfb0760b6eaca834cf8d108d4269aded4
sopesamuel <104572982+sopesamuel@users.noreply.github.com>
2022-07-03 22:08:04 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+/*const player = "per"
+const opponent = "nick"
+const game = "amazingFighter"
+let points = 0
+let hasWon = false
+
+points += 100
+hasWon = true
+
+if (hasWon){
+    console.log(`
+${[player]} got ${[points]} points and won the ${[game]} game!
+    `)
+} else{
+    console.log(`The winner is ${[opponent]} ! ${[player]} lost the game
+    `)
+}
+
+let myCourses = ["Learn CSS Animations", "UI Design Fundamentals", "Intro to clean code"]
+
+function logItems(arr){
+    for(let i = 0; i < myCourses.length; i++){
+        console.log(arr[i])
+    }
+}
+
+logItems(myCourses
+let myCredits = localStorage.getItem("mycredits")
+console.log(myCredits))
+
+const result = document.getElementById("score")
+let data = [
+    {
+        player: "Jane",
+        score: 52
+    }, 
+    {
+        player: "Mark",
+        score: 41
+    }
+]
+
+result.addEventListener("click", function(){
+console.log(data[0].score)
+}) 
+
+
+function generateSentence(desc, arr){
+let food = `The 2 ${[desc]} are ${[arr[0]]} and ${[arr[1]]}`
+for (let i = 0; i < arr.length; i++){
+    
+}
+console.log(food)
+}
+
+generateSentence("best fruits", ["apples", "oranges"])
+Use toFixed to round up decimals
+
+const totalPrice = "420.69235632455"
+const btn = document.getElementById("purchase-btn")
+
+btn.textContent = `Buy ${[Number(totalPrice).toFixed(3)]}`
+
+use Number( ) to convert number to string */
+
